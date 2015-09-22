984ae8bae584013fa5ef58bc30ce6e76628a5c0f
Enno Gröper <groepeen@cms.hu-berlin.de>
2015-09-22 17:30:23 +0200

    Fixed indentation (to make my editor happy)

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ __version__ = '4.4.0'
 
 
 setup(name='seafobj',
-        version=__version__,
-        license='BSD',
-        description='python library for accessing seafile data model',
-        author='Shuai Lin',
-        author_email='linshuai2012@gmail.com',
-        url='http://seafile.com',
-        platforms=['Any'],
-        packages=find_packages(),
-        classifiers=['Development Status :: 4 - Beta',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python'],
-        )
+      version=__version__,
+      license='BSD',
+      description='python library for accessing seafile data model',
+      author='Shuai Lin',
+      author_email='linshuai2012@gmail.com',
+      url='http://seafile.com',
+      platforms=['Any'],
+      packages=find_packages(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'])
