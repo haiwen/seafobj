9304ed9559c0e80897dc2e39920bbba8df87e28a
Jiaqiang Xu <xjqkilling@gmail.com>
2016-10-10 11:20:06 +0800

    [swift] Retry when token is expired.

--- seafobj/backends/swift.py
+++ seafobj/backends/swift.py
@@ -17,7 +17,7 @@ class SwiftConf(object):
         self.use_https = use_https
 
 class SeafSwiftClient(object):
-    MAX_RETRY = 3
+    MAX_RETRY = 2
 
     def __init__(self, swift_conf):
         self.swift_conf = swift_conf
@@ -39,53 +39,66 @@ class SeafSwiftClient(object):
         auth_data = {'auth': {'passwordCredentials': {'username': self.swift_conf.user_name,
                                                       'password': self.swift_conf.password},
                               'tenantName': self.swift_conf.tenant}}
-        i = 0
-        while i <= SeafSwiftClient.MAX_RETRY:
-            req = urllib2.Request(url, json.dumps(auth_data), hdr)
-            resp = urllib2.urlopen(req)
-            ret_code = resp.getcode()
-            ret_data = resp.read()
 
-            if ret_code == httplib.OK or ret_code == httplib.NON_AUTHORITATIVE_INFORMATION:
-                data_json = json.loads(ret_data)
-                self.token = data_json['access']['token']['id']
-                catalogs = data_json['access']['serviceCatalog']
-                for catalog in catalogs:
-                    if catalog['type'] == 'object-store':
-                        self.storage_url = catalog['endpoints'][0]['publicURL']
-                        return
-            elif ret_code == httplib.UNAUTHORIZED or ret_code >= httplib.INTERNAL_SERVER_ERROR:
-                # Retry
-                i += 1
-            else:
-                raise SwiftAuthenticateError('[swift] Failed to authenticate: [%d]%s' %
-                                             (ret_code, ret_data))
-        raise SwiftAuthenticateError('[swift] Failed to authenticate on retry %d times' %
-                                     SeafSwiftClient.MAX_RETRY)
+        req = urllib2.Request(url, json.dumps(auth_data), hdr)
+        try:
+            resp = urllib2.urlopen(req)
+        except urllib2.HTTPError as e:
+            raise SwiftAuthenticateError('[swift] Failed to authenticate: %d.' %
+                                         (SeafSwiftClient.MAX_RETRY, e.getcode()))
+        except urllib2.URLError as e:
+            raise SwiftAuthenticateError('[swift] Failed to authenticate: %s.' %
+                                         (SeafSwiftClient.MAX_RETRY, e.reason))
+
+        ret_code = resp.getcode()
+        ret_data = resp.read()
+
+        if ret_code == httplib.OK or ret_code == httplib.NON_AUTHORITATIVE_INFORMATION:
+            data_json = json.loads(ret_data)
+            self.token = data_json['access']['token']['id']
+            catalogs = data_json['access']['serviceCatalog']
+            for catalog in catalogs:
+                if catalog['type'] == 'object-store':
+                    self.storage_url = catalog['endpoints'][0]['publicURL']
+                    return
+        else:
+            raise SwiftAuthenticateError('[swift] Unexpected code when authenticate: %d' %
+                                         ret_code)
 
     def read_object_content(self, obj_id):
-        if not self.authenticated():
-            self.authenticate()
-
-        url = '%s/%s/%s' % (self.storage_url, self.swift_conf.container, obj_id)
-        hdr = {'X-Auth-Token': self.token}
         i = 0
         while i <= SeafSwiftClient.MAX_RETRY:
+            if not self.authenticated():
+                self.authenticate()
+
+            url = '%s/%s/%s' % (self.storage_url, self.swift_conf.container, obj_id)
+            hdr = {'X-Auth-Token': self.token}
             req = urllib2.Request(url, headers=hdr)
-            resp = urllib2.urlopen(req)
+            try:
+                resp = urllib2.urlopen(req)
+            except urllib2.HTTPError as e:
+                err_code = e.getcode()
+                if err_code == httplib.UNAUTHORIZED:
+                    # Reset token and storage_url
+                    self.token = None
+                    self.storage_url = None
+                    i += 1
+                    continue
+                else:
+                    raise GetObjectError('[swift] Failed to read %s: %d' % (obj_id, err_code))
+            except urllib2.URLError as e:
+                raise GetObjectError('[swift] Failed to read %s: %s' % (obj_id, e.reason))
+
             ret_code = resp.getcode()
             ret_data = resp.read()
 
             if ret_code == httplib.OK:
                 return ret_data
-            elif ret_code == httplib.UNAUTHORIZED or ret_code >= httplib.INTERNAL_SERVER_ERROR:
-                # Retry
-                i += 1
             else:
-                raise GetObjectError('[swift] Failed to read %s: [%d]%s' %
-                                     (obj_id, ret_code, ret_data))
-        raise GetObjectError('[swift] Failed to read %s on retry %d times' %
-                             (obj_id, SeafSwiftClient.MAX_RETRY))
+                raise GetObjectError('[swift] Unexpected code when read %s: %d' %
+                                     (obj_id, ret_code))
+        raise GetObjectError('[swift] Failed to read %s: quit after %d unauthorized retries.',
+                             SeafSwiftClient.MAX_RETRY)
 
 class SeafObjStoreSwift(AbstractObjStore):
     '''Swift backend for seafile objecs'''
