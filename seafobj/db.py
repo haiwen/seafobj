c96a62519b181e282188dc633494d92aefa56b4c
Heran Yang <heran.yang@seafile.com>
2026-03-27 06:27:30 +0000

    Read SEAFILE_DATA_DIR env

--- seafobj/db.py
+++ seafobj/db.py
@@ -34,11 +34,13 @@ def create_engine_from_conf(config):
     need_connection_pool_fix = True
 
     if not config.has_section('database'):
-        seafile_data_dir = os.environ['SEAFILE_CONF_DIR']
-        if seafile_data_dir:
+        seafile_data_dir = os.environ.get('SEAFILE_DATA_DIR','')
+        if seafile_data_dir == '':
+            seafile_data_dir = os.environ.get('SEAFILE_CONF_DIR', '')
+        if seafile_data_dir != '':
             path = os.path.join(seafile_data_dir, 'seafile.db')
         else:
-            logging.warning('SEAFILE_CONF_DIR not set, can not load sqlite database.')
+            logging.warning('SEAFILE_DATA_DIR not set, can not load database.')
             return None
         db_url = "sqlite:///%s" % path
         need_connection_pool_fix = False
